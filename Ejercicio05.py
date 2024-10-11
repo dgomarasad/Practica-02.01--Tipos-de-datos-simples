f2d8a64802e25f694b9f8133c8f45e0717398a77
dgomarasad <dgomarasad@educacion.navarra.es>
2024-10-11 22:38:59 +0200

    Create Ejercicio05.py

---
+++ Ejercicio05.py
@@ -0,0 +1,7 @@
+#Escribir un programa que pregunte al usuario por el número 
+# de horas trabajadasy el coste por hora.
+# Después debe mostrar por pantalla la paga que le corresponde.
+horas = float(input("Introduce tus horas de trabajo: "))
+coste = float(input("Introduce lo que cobras por hora: "))
+paga = horas * coste
+print("Tu paga es", paga)
